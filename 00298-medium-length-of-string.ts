7ad74185e3226f99afa7e4cb0f454f241384d8be
Paweł Sowa <pawel@the-sowa.com>
2022-07-02 11:07:43 +0200

    feat: length of string

---
+++ 00298-medium-length-of-string.ts
@@ -0,0 +1,17 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+    Expect<Equal<LengthOfString<"">, 0>>,
+    Expect<Equal<LengthOfString<"kumiko">, 6>>,
+    Expect<Equal<LengthOfString<"reina">, 5>>,
+    Expect<Equal<LengthOfString<"Sound! Euphonium">, 16>>
+];
+
+// ============= Your Code Here =============
+type LengthOfString<
+    S extends string,
+    R extends unknown[] = []
+> = S extends `${infer First}${infer Rest}`
+    ? LengthOfString<Rest, [First, ...R]>
+    : R["length"];
