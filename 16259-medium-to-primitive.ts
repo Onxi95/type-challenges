267c0020664cfd8ec7c64ed0ba48747745c303bb
Paweł Sowa <pawel@the-sowa.com>
2023-01-24 21:12:28 +0100

    feat: to primitive

---
+++ 16259-medium-to-primitive.ts
@@ -0,0 +1,42 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type PersonInfo = {
+  name: 'Tom'
+  age: 30
+  married: false
+  addr: {
+    home: '123456'
+    phone: '13111111111'
+  }
+  hobbies: ['sing', 'dance']
+}
+
+type ExpectedResult = {
+  name: string
+  age: number
+  married: boolean
+  addr: {
+    home: string
+    phone: string
+  }
+  hobbies: [string, string]
+}
+
+type cases = [
+  Expect<Equal<ToPrimitive<PersonInfo>, ExpectedResult>>,
+]
+
+
+// ============= Your Code Here =============
+type ToPrimitive<T> = {
+  [P in keyof T]:  T[P] extends string
+    ? string
+    : T[P] extends number
+    ? number
+    : T[P] extends boolean
+    ? boolean
+    : ToPrimitive<T[P]>
+}
+
+type Test1 = ToPrimitive<PersonInfo>
