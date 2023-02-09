3984b225ac0f54ef2f7b6df3463ed7e34242a2d5
Paweł Sowa <pawel@the-sowa.com>
2023-02-09 20:47:51 +0100

    feat: drop string

---
+++ 02059-hard-drop-string.ts
@@ -0,0 +1,33 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<DropString<'butter fly!', ''>, 'butter fly!'>>,
+  Expect<Equal<DropString<'butter fly!', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<'butter fly!', 'but'>, 'er fly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'but'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<'    butter fly!        ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', ' '>, 'butterfly!'>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'but'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'tub'>, '     e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 'b'>, '  u t t e r f l y ! '>>,
+  Expect<Equal<DropString<' b u t t e r f l y ! ', 't'>, ' b u   e r f l y ! '>>,
+]
+
+
+// ============= Your Code Here =============
+
+type StringToUnion<T extends string> = T extends `${infer First}${infer Rest}` ?
+  First | StringToUnion<Rest>
+  :
+  T
+
+type Test1 = StringToUnion<'Hello'>
+
+type DropString<S extends string, R extends string, T = StringToUnion<R>> = S extends `${infer First}${infer Rest}`
+  ? First extends T
+    ? DropString<Rest, R>
+    : `${First}${DropString<Rest, R>}`
+  : S
+
+type Test2 = DropString<'butter fly', 'but'>
