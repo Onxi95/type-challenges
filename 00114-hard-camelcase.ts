d50e5b87d39eb75217c09102a5dfc3648ae804bb
Paweł Sowa <pawel@the-sowa.com>
2023-02-11 11:29:53 +0100

    wip: camelcase

---
+++ 00114-hard-camelcase.ts
@@ -0,0 +1,22 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<CamelCase<'foobar'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'FOOBAR'>, 'foobar'>>,
+  Expect<Equal<CamelCase<'foo_bar'>, 'fooBar'>>,
+  Expect<Equal<CamelCase<'foo_bar_hello_world'>, 'fooBarHelloWorld'>>,
+  Expect<Equal<CamelCase<'HELLO_WORLD_WITH_TYPES'>, 'helloWorldWithTypes'>>,
+  Expect<Equal<CamelCase<'-'>, '-'>>,
+  Expect<Equal<CamelCase<''>, ''>>,
+  Expect<Equal<CamelCase<'😎'>, '😎'>>,
+]
+
+
+// ============= Your Code Here =============
+type CamelCase<S extends string> =
+  S extends `${infer First}_${infer Second}`
+  ? CamelCase<`${First}${Capitalize<Second>}`>
+  : S
+
+type Test1 = CamelCase<'foo_bar_hello_world'>
