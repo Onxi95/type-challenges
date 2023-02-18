686529f0ef9a08f98335d96081f903261ee2d744
Paweł Sowa <pawel@the-sowa.com>
2023-02-18 18:19:44 +0100

    feat: permutation

---
+++ 00296-medium-permutation.ts
@@ -0,0 +1,21 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type cases = [
+  Expect<Equal<Permutation<'A'>, ['A']>>,
+  Expect<Equal<Permutation<'A' | 'B' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<'B' | 'A' | 'C'>, ['A', 'B', 'C'] | ['A', 'C', 'B'] | ['B', 'A', 'C'] | ['B', 'C', 'A'] | ['C', 'A', 'B'] | ['C', 'B', 'A']>>,
+  Expect<Equal<Permutation<boolean>, [false, true] | [true, false]>>,
+  Expect<Equal<Permutation<never>, []>>,
+]
+
+
+// ============= Your Code Here =============
+type Permutation<T, U = T> = [T] extends [never] 
+  ? [] 
+  : T extends unknown
+  ? [T, ...Permutation<Exclude<U, T>>] 
+  : never;
+
+type test1 = Permutation<boolean>
+type test2 = Exclude<boolean, true>
