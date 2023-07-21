f88819af699cd432c7c7eca0d5a9f8e5a672e1df
Paweł Sowa <pawel@the-sowa.com>
2023-07-21 22:13:08 +0200

    feat: filter

---
+++ 18220-medium-filter.ts
@@ -0,0 +1,23 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type Falsy = false | 0 | '' | null | undefined
+
+type cases = [
+  Expect<Equal<Filter<[0, 1, 2], 2>, [2]>>,
+  Expect<Equal<Filter<[0, 1, 2], 0 | 1>, [0, 1]>>,
+  Expect<Equal<Filter<[0, 1, 2], Falsy>, [0]>>,
+]
+
+type ConvertArrayToUnion<T extends unknown[]> = T[number];
+
+// ============= Your Code Here =============
+type Filter<T extends any[], P,> =
+  T extends [infer First, ...infer Rest]
+  ?
+  First extends P
+  ? [First, ...Filter<Rest, P>]
+  : Filter<Rest, P>
+  : T;
+
+type test1 = Filter<[0, 1, 2], 2>
