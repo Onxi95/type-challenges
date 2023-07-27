f98060c37dc1cf157365d7a61b331731a55f74d7
Paweł Sowa <pawel@the-sowa.com>
2023-07-27 20:43:56 +0200

    feat: deeppick

---
+++ 00956-hard-deeppick.ts
@@ -0,0 +1,42 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from './test-utils'
+
+type Obj = {
+  a: number
+  b: string
+  c: boolean
+  obj: {
+    d: number
+    e: string
+    f: boolean
+    obj2: {
+      g: number
+      h: string
+      i: boolean
+    }
+  }
+  obj3: {
+    j: number
+    k: string
+    l: boolean
+  }
+}
+
+type cases = [
+  Expect<Equal<DeepPick<Obj, ''>, unknown>>,
+  Expect<Equal<DeepPick<Obj, 'a'>, { a: number }>>,
+  Expect<Equal<DeepPick<Obj, 'a' | ''>, { a: number } & unknown>>,
+  Expect<Equal<DeepPick<Obj, 'a' | 'obj.e'>, { a: number } & { obj: { e: string } }>>,
+  Expect<Equal<DeepPick<Obj, 'a' | 'obj.e' | 'obj.obj2.i'>, { a: number } & { obj: { e: string } } & { obj: { obj2: { i: boolean } } }>>,
+]
+
+
+// ============= Your Code Here =============
+
+type ParseStringAccessors<T extends string> = T extends `${infer First}.${infer Second}`
+  ? [First, ...ParseStringAccessors<Second>]
+  : [T];
+type DeepPick<T extends Record<string, unknown>, Props extends string> = T
+
+
+type test1 = ParseStringAccessors<'obj.obj2.i'>
