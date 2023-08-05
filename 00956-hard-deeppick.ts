faa7e9bfa233bfc47a20c175d190eb6f8302278e
Paweł Sowa <pawel@the-sowa.com>
2023-08-05 16:40:20 +0200

    wip: deeppick

--- 00956-hard-deeppick.ts
+++ 00956-hard-deeppick.ts
@@ -36,7 +36,19 @@ type cases = [
 type ParseStringAccessors<T extends string> = T extends `${infer First}.${infer Second}`
   ? [First, ...ParseStringAccessors<Second>]
   : [T];
-type DeepPick<T extends Record<string, unknown>, Props extends string> = T
+type DeepPick<T extends Record<string, unknown>, S extends string, Attrs = ParseStringAccessors<S>> =
+  Attrs extends [infer First extends string, ...infer Rest extends string[]]
+  ? T[First] extends Record<string, unknown>
+  ? Pick<T, First> & DeepPick<T[First], S, Rest>
+  : T
+  : {}
 
 
 type test1 = ParseStringAccessors<'obj.obj2.i'>
+type test2 = ParseStringAccessors<'a'>
+
+type test3 = DeepPick<Obj, 'a'>
+const Test3: test3 = { a: 123 }
+
+type test4 = DeepPick<Obj, 'a' | ''>
+const Test4: test4 = { a: 123 }
