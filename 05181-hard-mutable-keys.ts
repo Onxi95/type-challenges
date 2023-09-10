494a1d9f368e25803f12386107295586d24aa161
Paweł Sowa <pawel@the-sowa.com>
2023-09-10 17:12:32 +0200

    feat: mutable keys

---
+++ 05181-hard-mutable-keys.ts
@@ -0,0 +1,27 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<MutableKeys<{ a: number; readonly b: string }>, "a">>,
+  Expect<Equal<MutableKeys<{ a: undefined; readonly b: undefined }>, "a">>,
+  Expect<
+    Equal<
+      MutableKeys<{ a: undefined; readonly b?: undefined; c: string; d: null }>,
+      "a" | "c" | "d"
+    >
+  >,
+  Expect<Equal<MutableKeys<{}>, never>>
+];
+
+// ============= Your Code Here =============
+type MutableKeys<T> = keyof {
+  [Key in keyof T as Equal<Pick<T, Key>, Readonly<Pick<T, Key>>> extends true
+    ? never
+    : Key]: Key;
+};
+
+type test1 = MutableKeys<{ a: number; readonly b: string }>;
+type test2 = MutableKeys<{ a: undefined; readonly b: undefined }>;
+
+type test = { readonly a: "a" };
+type testPick = Pick<test, "a">;
