c64f4b4d1bc954cee33ebe7bd8250dbc8faff201
Paweł Sowa <pawel@the-sowa.com>
2023-09-16 12:23:16 +0200

    wip: GetFirstNItems, OffsetArrayBy

---
+++ 00216-extreme-slice.ts
@@ -0,0 +1,64 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type Arr = [1, 2, 3, 4, 5];
+
+type cases = [
+  // basic
+  Expect<Equal<Slice<Arr, 0, 1>, [1]>>,
+  Expect<Equal<Slice<Arr, 0, 0>, []>>,
+  Expect<Equal<Slice<Arr, 2, 4>, [3, 4]>>,
+
+  // optional args
+  Expect<Equal<Slice<[]>, []>>,
+  Expect<Equal<Slice<Arr>, Arr>>,
+  Expect<Equal<Slice<Arr, 0>, Arr>>,
+  Expect<Equal<Slice<Arr, 2>, [3, 4, 5]>>,
+
+  // negative index
+  Expect<Equal<Slice<Arr, 0, -1>, [1, 2, 3, 4]>>,
+  Expect<Equal<Slice<Arr, -3, -1>, [3, 4]>>,
+
+  // invalid
+  Expect<Equal<Slice<Arr, 10>, []>>,
+  Expect<Equal<Slice<Arr, 1, 0>, []>>,
+  Expect<Equal<Slice<Arr, 10, 20>, []>>
+];
+
+// ============= Your Code Here =============
+
+type GetFirstNItems<
+  Arr extends unknown[],
+  N extends number,
+  Result extends unknown[] = []
+> = Result["length"] extends N
+  ? Result
+  : Arr extends [infer First, ...infer Rest]
+  ? GetFirstNItems<Rest, N, [...Result, First]>
+  : Result;
+
+type OffsetArrayBy<
+  Arr extends unknown[],
+  OffsetIdx extends number,
+  Result extends unknown[] = [],
+  Memory extends unknown[] = []
+> = Memory["length"] extends OffsetIdx
+  ? Result
+  : Arr extends [infer First, ...infer Rest]
+  ? true
+  : Result;
+
+type Slice<
+  Arr extends unknown[],
+  Start extends number = 0,
+  End extends number = Arr["length"],
+  Result extends unknown[] = [],
+  Iterator extends unknown[] = []
+> = Arr extends [infer First, ...infer Rest] ? true : never;
+
+type test1 = Slice<Arr, 0, 1>;
+
+type test2 = GetFirstNItems<Arr, 0>;
+type test3 = GetFirstNItems<[5, 4, 3, 2, 1], 3>;
+
+type test4 = OffsetArrayBy<Arr, 3>;
