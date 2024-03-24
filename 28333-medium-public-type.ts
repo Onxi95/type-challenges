d50ba7d7901f7c2805f048d984e9199502ebdc1b
Paweł Sowa <pawel@the-sowa.com>
2024-03-24 11:49:39 +0100

    feat: public type

---
+++ 28333-medium-public-type.ts
@@ -0,0 +1,17 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<PublicType<{ a: number }>, { a: number }>>,
+  Expect<Equal<PublicType<{ _b: string | bigint }>, {}>>,
+  Expect<Equal<PublicType<{ readonly c?: number }>, { readonly c?: number }>>,
+  Expect<Equal<PublicType<{ d: string; _e: string }>, { d: string }>>,
+  Expect<Equal<PublicType<{ _f: () => bigint[] }>, {}>>,
+  Expect<Equal<PublicType<{ g: "_g" }>, { g: "_g" }>>,
+  Expect<Equal<PublicType<{ __h: number; i: unknown }>, { i: unknown }>>
+];
+
+// ============= Your Code Here =============
+type PublicType<T extends object> = {
+  [Key in keyof T as Key extends `_${string}` ? never : Key]: T[Key];
+};
