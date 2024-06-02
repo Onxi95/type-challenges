f730b5673a428dbdf5d93de2a6b49c3bb74d6774
Paweł Sowa <pawel@the-sowa.com>
2024-06-02 10:27:55 +0200

    refactor: adjust First to `PropertyKey`

--- 03188-medium-tuple-to-nested-object.ts
+++ 03188-medium-tuple-to-nested-object.ts
@@ -1,20 +1,26 @@
 // ============= Test Cases =============
-import type { Equal, Expect } from './test-utils'
+import type { Equal, Expect } from "./test-utils";
 
 type cases = [
-  Expect<Equal<TupleToNestedObject<['a'], string>, { a: string }>>,
-  Expect<Equal<TupleToNestedObject<['a', 'b'], number>, { a: { b: number } }>>,
-  Expect<Equal<TupleToNestedObject<['a', 'b', 'c'], boolean>, { a: { b: { c: boolean } } }>>,
-  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>,
-]
-
+  Expect<Equal<TupleToNestedObject<["a"], string>, { a: string }>>,
+  Expect<Equal<TupleToNestedObject<["a", "b"], number>, { a: { b: number } }>>,
+  Expect<
+    Equal<
+      TupleToNestedObject<["a", "b", "c"], boolean>,
+      { a: { b: { c: boolean } } }
+    >
+  >,
+  Expect<Equal<TupleToNestedObject<[], boolean>, boolean>>
+];
 
 // ============= Your Code Here =============
-type TupleToNestedObject<T extends string[], U> =
-  T extends [infer First extends string, ...infer Rest extends string[]]
+type TupleToNestedObject<T, U> = T extends [
+  infer First extends PropertyKey,
+  ...infer Rest
+]
   ? Record<First, TupleToNestedObject<Rest, U>>
   : U;
 
-type test1 = TupleToNestedObject<[], boolean>
-type test2 = TupleToNestedObject<['a', 'b', 'c'], boolean>
-type test3 = TupleToNestedObject<['a'], boolean>
+type test1 = TupleToNestedObject<[], boolean>;
+type test2 = TupleToNestedObject<["a", "b", "c"], boolean>;
+type test3 = TupleToNestedObject<["a"], boolean>;
